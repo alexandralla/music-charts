622a9c939628b81c8fa6008fd6a2c90e3a28baf0
Alex <alex_llamas@icloud.com>
2020-02-29 16:31:38 -0800

    connected to html

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 const { getChart } = require('./billboard-top-100.js');
 
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url)
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('head ass')
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/headass.html', 'utf8');
+    myReadStream.pipe(res);
+    // res.end('head ass')
 });
 
 getChart('hot-100', (err, chart) => {
