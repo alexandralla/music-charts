f32125bf20f6f098c400e823224c0aea3e1fb9e9
Satvir Singh <satvir@pdx.edu>
2020-03-03 14:47:39 -0800

    Writing to json file

--- app2.js
+++ app2.js
@@ -27,3 +27,28 @@ app.get('/', (req, res) => {
 
 app.listen(8080);
 console.log('listening to port 8080');
+
+getChart('hot-100', (err, chart) => {
+  if (err) console.log(err);
+  roddyRicch = chart.songs[0];
+  console.log(JSON.stringify(roddyRicch));
+  var data = JSON.stringify(roddyRicch);
+  var fs = require('fs');
+fs.writeFile("test.json", data, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+});
+
+function done() {
+  // var roddyRicch;
+  //   getChart('hot-100', (err, chart) => {
+  //     if (err) console.log(err);
+  //     roddyRicch = chart.songs[0];
+  //     console.log(JSON.stringify(roddyRicch));
+  //   });
+  return {
+    rank: 1, title: 'The Box', artist: 'Roddy Ricch', cover: 'https://charts-static.billboard.com/img/2019/12/roddy-ricch-eqp-the-box-2k7-53x53.jpg', position: { positionLastWeek: 1, peakPosition: 1, weeksOnChart: 11 },
+  };
+}
