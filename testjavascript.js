f84ec5e86576c88ec184bb411a4d60c9039975b1
Satvir Singh <satvir@pdx.edu>
2020-03-07 22:59:00 -0800

    Added third function

--- testjavascript.js
+++ testjavascript.js
@@ -7,8 +7,12 @@
     function testfunc2(x){
         return x[1];
     }
+    function testfunc3(x){
+        return x[2];
+    }
     exports.testfunc = testfunc;
     exports.testfunc2 = testfunc2;
+    exports.testfunc3 = testfunc3;
        
 })(typeof exports === 'undefined'?  
             this['testjavascript']={}: exports); 
