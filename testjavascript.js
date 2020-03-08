17f43949f46746886fa445783b2770a6e43cf602
Satvir Singh <satvir@pdx.edu>
2020-03-08 04:29:56 -0700

    Added functions to compute top ten songs of the top 100 sidebar

--- testjavascript.js
+++ testjavascript.js
@@ -10,9 +10,37 @@
     function testfunc3(x){
         return x[2];
     }
+    function testfunc4(x){
+        return x[3];
+    }
+    function testfunc5(x){
+        return x[4];
+    }
+    function testfunc6(x){
+        return x[5];
+    }
+    function testfunc7(x){
+        return x[6];
+    }
+    function testfunc8(x){
+        return x[7];
+    }
+    function testfunc9(x){
+        return x[8];
+    }
+    function testfunc10(x){
+        return x[9];
+    }
     exports.testfunc = testfunc;
     exports.testfunc2 = testfunc2;
     exports.testfunc3 = testfunc3;
+    exports.testfunc4 = testfunc4;
+    exports.testfunc5 = testfunc6;
+    exports.testfunc6 = testfunc6;
+    exports.testfunc7 = testfunc7;
+    exports.testfunc8 = testfunc8;
+    exports.testfunc9 = testfunc9;
+    exports.testfunc10 = testfunc10;
        
 })(typeof exports === 'undefined'?  
             this['testjavascript']={}: exports); 
