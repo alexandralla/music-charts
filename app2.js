740b1d1604effce28939f13150c4ec6f1638ac68
Satvir Singh <satvir@pdx.edu>
2020-03-08 04:28:30 -0700

    Updated top ten songs on top 100 sidebar

--- app2.js
+++ app2.js
@@ -28,9 +28,17 @@ function getDate(timeShift) {
 // CREATES TODAY SERVER
 app.get('/', (req, res) => {
   res.render('music-charts', {topsongtitle: topsong.title, topsongartist: topsong.artist, topsongweeks: topsong.position.weeksOnChart, 
-    top100title1: topsong.title, top100artist1: topsong.artist, top100weeks1: topsong.position.weeksOnChart, 
-    top100title2: secondsong.title, top100artist2: secondsong.artist, top100weeks2: secondsong.position.weeksOnChart,
-    top100title3: thirdsong.title, top100artist3: thirdsong.artist, top100weeks3: thirdsong.position.weeksOnChart});
+    top100title1: topsong.title, top100artist1: topsong.artist, top100weeks1: topsong.position.weeksOnChart, top100image1: topsong.cover, 
+    top100title2: secondsong.title, top100artist2: secondsong.artist, top100weeks2: secondsong.position.weeksOnChart, top100image2: secondsong.cover,
+    top100title3: thirdsong.title, top100artist3: thirdsong.artist, top100weeks3: thirdsong.position.weeksOnChart, top100image3: thirdsong.cover,
+    top100title4: fourthsong.title, top100artist4: fourthsong.artist, top100weeks4: fourthsong.position.weeksOnChart, top100image4: fourthsong.cover,
+    top100title5: fifthsong.title, top100artist5: fifthsong.artist, top100weeks5: fifthsong.position.weeksOnChart, top100image5: fifthsong.cover,
+    top100title6: sixthsong.title, top100artist6: sixthsong.artist, top100weeks6: sixthsong.position.weeksOnChart, top100image6: sixthsong.cover,
+    top100title7: seventhsong.title, top100artist7: seventhsong.artist, top100weeks7: seventhsong.position.weeksOnChart, top100image7: seventhsong.cover,
+    top100title8: eigthsong.title, top100artist8: eigthsong.artist, top100weeks8: eigthsong.position.weeksOnChart, top100image8: eigthsong.cover,
+    top100title9: ninthsong.title, top100artist9: ninthsong.artist, top100weeks9: ninthsong.position.weeksOnChart, top100image9: ninthsong.cover,
+    top100title10: tenthsong.title, top100artist10: tenthsong.artist, top100weeks10: tenthsong.position.weeksOnChart, top100image10: tenthsong.cover});
+    
   //console.log(`request was made: ${req.url}`);
   //res.sendFile(`${__dirname}/music-charts.html`);
 });
@@ -220,11 +228,24 @@ fs.readFile('today.json', (err, data) => {
   topsong = utilities.testfunc(chart);
   secondsong = utilities.testfunc2(chart);
   thirdsong = utilities.testfunc3(chart);
+  fourthsong = utilities.testfunc4(chart);
+  fifthsong = utilities.testfunc5(chart);
+  sixthsong = utilities.testfunc6(chart);
+  seventhsong = utilities.testfunc7(chart);
+  eigthsong = utilities.testfunc8(chart);
+  ninthsong = utilities.testfunc9(chart);
+  tenthsong = utilities.testfunc10(chart);
   console.log(topsong);
   console.log(secondsong);
   console.log(thirdsong);
+  console.log(fourthsong);
+  console.log(fifthsong);
+  console.log(sixthsong);
+  console.log(seventhsong);
+  console.log(eigthsong);
+  console.log(ninthsong);
+  console.log(tenthsong);
 });
-
 
 app.listen(8080);
 console.log('listening to port 8080');
